31c1fbaa5d49fe3be01d07c873e933806e40087e
wang790222 <wang790222@gmail.com>
2019-02-20 21:00:01 +0000

    Login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,21 @@ class SessionsController < ApplicationController
   end
 
   def create
-
-    #render 'new'
+    #@session = User.find(session_params)
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      redirect_to :root
+    else
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new'
+    end
   end
 
   def destroy
   end
+
+  private
+    def session_params
+      params.require(:session).permit(:email, :password)
+    end
 end
